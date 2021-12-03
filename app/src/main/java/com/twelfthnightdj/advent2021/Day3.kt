a657a4b8f830078f36b92fc04cea90d9405a0ebd
Rolando Lopez <rolando.lopez@milwaukeetool.com>
2021-12-03 11:19:32 -0600

    Day 3 Part 2

--- app/src/main/java/com/twelfthnightdj/advent2021/Day3.kt
+++ app/src/main/java/com/twelfthnightdj/advent2021/Day3.kt
@@ -17,8 +17,40 @@ class Day3 : AocDays() {
         }
         val gDec = gamma.asList().joinToString("").toInt(2)
         val eDec = epsilon.asList().joinToString("").toInt(2)
-        println("answer: ${(gDec * eDec)}")
         return "${(gDec * eDec)}"
+    }
+
+    override fun partB(): String {
+        return partBHelper(input)
+    }
+
+    private fun partBHelper(ipt: List<String>): String {
+        val oxygen = criteria(ipt = ipt, checkingFor = '1', more = '1', less = '0', place = 0, default = '1')
+        val scrubber = criteria(ipt = ipt, checkingFor = '1', more = '0', less = '1', place = 0, default = '0')
+        val oDec = oxygen.joinToString("").toInt(2)
+        val sDec = scrubber.joinToString("").toInt(2)
+        return "${(oDec * sDec)}"
+    }
+    private fun criteria(ipt: List<String>, checkingFor: Char, more: Char, less: Char, place: Int, default: Char): List<String> {
+        val onesCount = ipt.map { it[place] }.filter { it == checkingFor }.size
+        val winner = when {
+            (onesCount < (ipt.size / 2.0)) -> less
+            (onesCount > (ipt.size / 2.0)) -> more
+            else -> default
+        }
+        val sifted = ipt.filter { it[place] == winner }
+        return if (sifted.size == 1) {
+            sifted
+        } else {
+            criteria(
+                ipt = sifted,
+                checkingFor = checkingFor,
+                more = more,
+                less = less,
+                place = place + 1,
+                default = default
+            )
+        }
     }
 
     val trialInput = listOf(
