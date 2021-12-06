5ec374186091429fe7058f0fe410ca83112b8086
Rolando Lopez <rolando.lopez@milwaukeetool.com>
2021-12-06 10:09:32 -0600

    Day 6 Part 2 Thanks to Michael Fazio for this one.

--- app/src/main/java/com/twelfthnightdj/advent2021/day06/Day6.kt
+++ app/src/main/java/com/twelfthnightdj/advent2021/day06/Day6.kt
@@ -6,14 +6,18 @@ import com.twelfthnightdj.advent2021.util.InputHelpers
 class Day6 : AocDays() {
     override fun partA(): String {
         val seed = input?.split(",") ?: emptyList()
-        return start(seed.map { it.toInt() })
+        return  start(seed.map { it.toInt() })
     }
 
+    override fun partB(): String {
+        val seed = input?.split(",") ?: emptyList()
+        return fazio(seed.map { it.toInt() }, 256)
+    }
 
     private fun start(seed: List<Int>, days: Int = 80): String {
         val aquarium = mutableListOf<Fish>()
         aquarium.addAll(seed.map { Fish(it) })
-        repeat(days) {
+        repeat(days) { day ->
             var newFishes = 0
             aquarium.forEach { fish ->
                 if (fish.nextDay()) {
@@ -27,6 +31,16 @@ class Day6 : AocDays() {
         return aquarium.count().toString()
     }
 
+    private fun fazio(seed: List<Int>, days: Int = 80): String {
+        val fishCounts = (0..8).map { ind -> seed.count { it == ind }.toLong() }
+        val resultFish = (1..days).fold(fishCounts) { counts, _ ->
+            val newFish = counts[0]
+            counts.drop(1).toMutableList().also {
+                it[6] += newFish
+            } + newFish
+        }
+        return resultFish.sum().toString()
+    }
 
     val trialInput = InputHelpers.getContentsFromFile("/day06trial.txt")
     val input = InputHelpers.getContentsFromFile("/day06.txt")
