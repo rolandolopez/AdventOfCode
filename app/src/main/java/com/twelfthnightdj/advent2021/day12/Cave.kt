971771925977ac38796fd5d585943c4062cf5325
Rolando Lopez <rolando.lopez@milwaukeetool.com>
2021-12-12 21:25:48 -0600

    Day 12 Part 2 attempt

--- app/src/main/java/com/twelfthnightdj/advent2021/day12/Cave.kt
+++ app/src/main/java/com/twelfthnightdj/advent2021/day12/Cave.kt
@@ -5,6 +5,16 @@ class Cave (val name: String) {
     val canRevisit: Boolean
         get() = name.first().isUpperCase()
 
+    var visitedState: Day12.VisitedStatus = if (name in listOf("start", "end")) Day12.VisitedStatus.DONE else Day12.VisitedStatus.NEVER
+
+    fun visit() {
+        if (visitedState == Day12.VisitedStatus.NEVER) {
+            visitedState = Day12.VisitedStatus.ONCE
+        } else if (visitedState == Day12.VisitedStatus.ONCE) {
+            visitedState = Day12.VisitedStatus.DONE
+        }
+    }
+
     fun addConnection(secondCave: Cave) {
         connections.add(secondCave)
     }
@@ -20,6 +30,6 @@ class Cave (val name: String) {
     }
 
     override fun toString(): String {
-        return "Cave $name ($canRevisit)with connections to ${connections.map { it.name }}"
+        return "Cave $name"// ($canRevisit)with connections to ${connections.map { it.name }}"
     }
 }
--- app/src/main/java/com/twelfthnightdj/advent2021/day12/Day12.kt
+++ app/src/main/java/com/twelfthnightdj/advent2021/day12/Day12.kt
@@ -9,10 +9,23 @@ class Day12 : AocDays() {
     private var pathCounter = 0
 
     override fun partA(): String {
-        processInput(input)
+        processInput(trialInput)
         val startCave = caves.values.first { it.name == "start" }
         val endCave = caves.values.first { it.name == "end" }
-        printAllPaths(startCave, endCave)
+        printAllPathsA(startCave, endCave)
+        return "$pathCounter"
+    }
+
+    override fun reset() {
+        pathCounter = 0
+        caves.clear()
+    }
+
+    override fun partB(): String {
+        processInput(trialInput)
+        val startCave = caves.values.first { it.name == "start" }
+        val endCave = caves.values.first { it.name == "end" }
+        printAllPathsB(startCave, endCave)
         return "$pathCounter"
     }
 
@@ -21,21 +34,38 @@ class Day12 : AocDays() {
             val (first, second) = line.split("-")
             val firstCave = caves.getOrDefault(first, Cave(first))
             val secondCave = caves.getOrDefault(second, Cave(second))
-            firstCave.addConnection(secondCave)
-            secondCave.addConnection(firstCave)
+            if (secondCave.name != "start") {
+                if (firstCave.name != "end") {
+                    firstCave.addConnection(secondCave)
+                }
+            }
+            if (firstCave.name != "start") {
+                if (secondCave.name != "end") {
+                    secondCave.addConnection(firstCave)
+                }
+            }
             caves[first] = firstCave
             caves[second] = secondCave
         }
     }
 
-    private fun printAllPaths(s: Cave, d: Cave) {
+    private fun printAllPathsA(s: Cave, d: Cave) {
         val isVisited = mutableSetOf<Cave>()
         val pathList = mutableListOf<Cave>()
         pathList.add(s)
-        printAllPathsUtil(s, d, isVisited, pathList)
+        printAllPathsUtilA(s, d, isVisited, pathList)
     }
 
-    private fun printAllPathsUtil(
+    private fun printAllPathsB(s: Cave, d: Cave) {
+        val isVisited = mutableSetOf<Cave>()
+        val pathList = mutableListOf<Cave>()
+        var hasVisitedTwice = false
+        pathList.add(s)
+        println("caves: $caves: $pathList")
+        printAllPathsUtilB(s, d, isVisited, pathList, false)
+    }
+
+    private fun printAllPathsUtilA(
         u: Cave,
         d: Cave,
         isVisited: MutableSet<Cave>,
@@ -50,7 +80,7 @@ class Day12 : AocDays() {
         u.connections.forEach { connectedCave ->
             if (!isVisited.contains(connectedCave)) {
                 localPathList.add(connectedCave)
-                printAllPathsUtil(connectedCave, d, isVisited, localPathList)
+                printAllPathsUtilA(connectedCave, d, isVisited, localPathList)
 
                 localPathList.remove(connectedCave)
             }
@@ -58,6 +88,51 @@ class Day12 : AocDays() {
         isVisited.remove(u)
     }
 
+    private fun printAllPathsUtilB(
+        u: Cave,
+        d: Cave,
+        isVisited: MutableSet<Cave>,
+        localPathList: MutableList<Cave>,
+        hasVisitedTwice: Boolean
+    ) {
+        if (u == d) {
+            println("list: $localPathList")
+            pathCounter++
+        }
+        if (!u.canRevisit) {
+            isVisited.add(u)
+        }
+        u.connections.forEach { connectedCave ->
+            if (connectedCave.name == "c") {
+                println("yes")
+            }
+            if (!isVisited.contains(connectedCave) || (isVisited.contains(connectedCave) && !hasVisitedTwice)) {
+                println("connectCave: $connectedCave")
+                println("${!isVisited.contains(connectedCave)} || (${isVisited.contains(connectedCave)} && ${!hasVisitedTwice})")
+                var twice = hasVisitedTwice
+                if (isVisited.contains(connectedCave)) {
+                    twice = true
+                }
+
+                localPathList.add(connectedCave)
+
+                printAllPathsUtilB(connectedCave, d, isVisited, localPathList, twice)
+
+                localPathList.removeLast()
+//                localPathList.remove(connectedCave)
+            }
+        }
+        println("isVisited before: $isVisited")
+        isVisited.remove(u)
+        println("isVisited after: $isVisited")
+    }
+
     val trialInput = InputHelpers.getListOfStringsFromFile("/day12trial.txt")
     private val input = InputHelpers.getListOfStringsFromFile("/day12.txt")
+
+    enum class VisitedStatus {
+        NEVER,
+        ONCE,
+        DONE
+    }
 }
