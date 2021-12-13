17744ff07db5eb0f08f3dc6de84a30dcf9ac943d
Rolando Lopez <rolando.lopez@milwaukeetool.com>
2021-12-12 23:08:40 -0600

    Day 12 Part 2 attempt with A LOT of help

--- app/src/main/java/com/twelfthnightdj/advent2021/day12/Day12.kt
+++ app/src/main/java/com/twelfthnightdj/advent2021/day12/Day12.kt
@@ -1,5 +1,6 @@
 package com.twelfthnightdj.advent2021.day12
 
+import android.util.MutableBoolean
 import com.twelfthnightdj.advent2021.AocDays
 import com.twelfthnightdj.advent2021.util.InputHelpers
 
@@ -7,6 +8,7 @@ class Day12 : AocDays() {
 
     private var caves = mutableMapOf<String, Cave>()
     private var pathCounter = 0
+    private var doubleVisit = MutableBoolean(false)
 
     override fun partA(): String {
         processInput(trialInput)
@@ -21,12 +23,10 @@ class Day12 : AocDays() {
         caves.clear()
     }
 
+    @ExperimentalStdlibApi
     override fun partB(): String {
-        processInput(trialInput)
-        val startCave = caves.values.first { it.name == "start" }
-        val endCave = caves.values.first { it.name == "end" }
-        printAllPathsB(startCave, endCave)
-        return "$pathCounter"
+
+        return "${calculateAll(parse(input), canVisitTwice = true)}"
     }
 
     private fun processInput(ipt: List<String>) {
@@ -62,7 +62,7 @@ class Day12 : AocDays() {
         var hasVisitedTwice = false
         pathList.add(s)
         println("caves: $caves: $pathList")
-        printAllPathsUtilB(s, d, isVisited, pathList, false)
+        printAllPathsUtilB(s, d, isVisited, pathList, doubleVisit)
     }
 
     private fun printAllPathsUtilA(
@@ -93,7 +93,7 @@ class Day12 : AocDays() {
         d: Cave,
         isVisited: MutableSet<Cave>,
         localPathList: MutableList<Cave>,
-        hasVisitedTwice: Boolean
+        hasVisitedTwice: MutableBoolean
     ) {
         if (u == d) {
             println("list: $localPathList")
@@ -103,29 +103,49 @@ class Day12 : AocDays() {
             isVisited.add(u)
         }
         u.connections.forEach { connectedCave ->
-            if (connectedCave.name == "c") {
-                println("yes")
-            }
-            if (!isVisited.contains(connectedCave) || (isVisited.contains(connectedCave) && !hasVisitedTwice)) {
+            if (!isVisited.contains(connectedCave) || (isVisited.contains(connectedCave) && !hasVisitedTwice.value)) {
                 println("connectCave: $connectedCave")
-                println("${!isVisited.contains(connectedCave)} || (${isVisited.contains(connectedCave)} && ${!hasVisitedTwice})")
-                var twice = hasVisitedTwice
+                println("${!isVisited.contains(connectedCave)} || (${isVisited.contains(connectedCave)} && ${!hasVisitedTwice.value})")
                 if (isVisited.contains(connectedCave)) {
-                    twice = true
+                    hasVisitedTwice.value = true
                 }
 
                 localPathList.add(connectedCave)
 
-                printAllPathsUtilB(connectedCave, d, isVisited, localPathList, twice)
+                printAllPathsUtilB(connectedCave, d, isVisited, localPathList, hasVisitedTwice)
 
-                localPathList.removeLast()
+                val last = localPathList.removeLast()
+                if (!last.canRevisit && localPathList.contains(last)) {
+                    println("changing hasVisitedTwice back to false last: $last path: $localPathList")
+                    hasVisitedTwice.value = false
+                }
 //                localPathList.remove(connectedCave)
             }
         }
-        println("isVisited before: $isVisited")
         isVisited.remove(u)
-        println("isVisited after: $isVisited")
     }
+
+    @ExperimentalStdlibApi
+    fun parse(ipt: List<String>) = buildMap<String, Iterable<String>> {
+        ipt.map { it.split("-") }
+            .forEach { (v1, v2) ->
+                compute(v1) { _, l-> (l ?: hashSetOf()) + v2 }
+                compute(v2) { _, l-> (l ?: hashSetOf()) + v1 }
+            }
+    }
+
+    fun calculateAll(
+        edges: Map<String, Iterable<String>>,
+        current: String = "start",
+        visited: Collection<String> = hashSetOf(current),
+        canVisitTwice: Boolean = false
+    ): Int = edges[current]!!.sumOf { next -> when {
+        next == "end" -> 1
+        next.first().isUpperCase() -> calculateAll(edges, next, visited + current, canVisitTwice)
+        next !in visited -> calculateAll(edges, next, visited + current, canVisitTwice)
+        canVisitTwice && next != "start" -> calculateAll(edges, next, visited + current, false)
+        else -> 0
+    } }
 
     val trialInput = InputHelpers.getListOfStringsFromFile("/day12trial.txt")
     private val input = InputHelpers.getListOfStringsFromFile("/day12.txt")
