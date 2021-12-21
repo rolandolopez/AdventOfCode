ce814a7f1ad70e8e15aba7f25eca54298ce63159
Rolando Lopez <rolando.lopez@milwaukeetool.com>
2021-12-21 08:57:40 -0600

    Day 21 Part 1

--- app/src/main/java/com/twelfthnightdj/advent2021/FirstFragment.kt
+++ app/src/main/java/com/twelfthnightdj/advent2021/FirstFragment.kt
@@ -28,6 +28,7 @@ import com.twelfthnightdj.advent2021.day16.Day16
 import com.twelfthnightdj.advent2021.day17.Day17
 import com.twelfthnightdj.advent2021.day18.Day18
 import com.twelfthnightdj.advent2021.day20.Day20
+import com.twelfthnightdj.advent2021.day21.Day21
 import java.lang.Integer.min
 import java.util.*
 import java.util.Calendar.DAY_OF_MONTH
@@ -90,6 +91,7 @@ class FirstFragment : Fragment(), AdapterView.OnItemSelectedListener {
                 17 -> runDay(Day17())
                 18 -> runDay(Day18())
                 20 -> runDay(Day20())
+                21 -> runDay(Day21())
 
                 else -> {}
             }
