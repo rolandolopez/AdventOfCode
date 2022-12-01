61253ce70011d140258754fb34b5a55ffa710ff2
Rolando Lopez <rolando.lopez@milwaukeetool.com>
2022-12-01 08:27:26 -0600

    More efficient

--- app/src/main/java/advent2022/day01/Y22D01.kt
+++ app/src/main/java/advent2022/day01/Y22D01.kt
@@ -5,22 +5,11 @@ import com.twelfthnightdj.advent2021.AocDays
 class Y22D01 : AocDays() {
     override var dayId = 1
 
-    override fun partA(): String {
-        var highestTotal = 0
-        var localTotal = 0
-        input.forEach { calories ->
-            if (calories.isEmpty()) {
-                highestTotal = maxOf(highestTotal, localTotal)
-                localTotal = 0
-            } else {
-                localTotal += calories.toInt()
-            }
+    override fun partA() = process(1)
 
-        }
-        return "$highestTotal"
-    }
+    override fun partB() = process(3)
 
-    override fun partB(): String {
+    private fun process(limit: Int): String {
         var localTotal = 0
         val elves = mutableListOf<Int>()
         input.forEach { calories ->
@@ -32,7 +21,6 @@ class Y22D01 : AocDays() {
             }
         }
         elves.add(localTotal)
-        val topThree = elves.sortedDescending().take(3).sum()
-        return "$topThree"
+        return "${elves.sortedDescending().take(limit).sum()}"
     }
 }
