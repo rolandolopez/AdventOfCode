2d98febd55ffa9faa9c7e251ce51df9b2270a51d
Rolando Lopez <rolando.lopez@milwaukeetool.com>
2022-12-02 08:36:41 -0600

    Day 2 Part B

--- app/src/main/java/advent2022/day02/Y22D02.kt
+++ app/src/main/java/advent2022/day02/Y22D02.kt
@@ -4,25 +4,39 @@ import com.twelfthnightdj.advent2021.AocDays
 
 class Y22D02 :AocDays() {
     override var dayId = 2
+    var total = 0
+    var rounds = mutableListOf<Pair<String, String>>()
 
-    override fun partA(): String {
-        val rounds = mutableListOf<Pair<String, String>>()
+    override fun setup() {
         input.forEach { round ->
             val hands = round.split(" ")
             rounds.add(Pair(hands[0], hands[1]))
         }
-        var total = 0
+    }
+
+    init {
+        setup()
+    }
+
+    override fun partA(): String {
         rounds.forEach {
-            total += fight(it.first, it.second)
+            total += fightA(it.first, it.second)
         }
         return "$total"
     }
 
-    override fun partB(): String {
-        return "part 2"
+    override fun reset() {
+        total = 0
     }
 
-    private fun fight(opponent: String, mine: String): Int {
+    override fun partB(): String {
+        rounds.forEach {
+            total += fightB(it.first, it.second)
+        }
+        return "$total"
+    }
+
+    private fun fightA(opponent: String, mine: String): Int {
         return when (opponent) {
             //rock
             "A" -> when (mine) {
@@ -43,6 +57,33 @@ class Y22D02 :AocDays() {
                 "X" -> 7 // 6 + 1
                 "Y" -> 2 // 0 + 2
                 "Z" -> 6 // 3 + 3
+                else -> 0
+            }
+            else -> 0
+        }
+
+    }
+    private fun fightB(opponent: String, mine: String): Int {
+        return when (opponent) {
+            //rock
+            "A" -> when (mine) {
+                "X" -> 3 // 0 + 3
+                "Y" -> 4 // 3 + 1
+                "Z" -> 8 // 6 + 2
+                else -> 0
+            }
+            // paper
+            "B" -> when (mine) {
+                "X" -> 1 // 0 + 1
+                "Y" -> 5 // 3 + 2
+                "Z" -> 9 // 6 + 3
+                else -> 0
+            }
+            // scissor
+            "C" -> when (mine) {
+                "X" -> 2 // 0 + 2
+                "Y" -> 6 // 3 + 3
+                "Z" -> 7 // 6 + 1
                 else -> 0
             }
             else -> 0
--- app/src/main/java/com/twelfthnightdj/advent2021/AocDays.kt
+++ app/src/main/java/com/twelfthnightdj/advent2021/AocDays.kt
@@ -5,9 +5,11 @@ import com.twelfthnightdj.advent2021.util.InputHelpers
 abstract class AocDays {
     open var dayId = 0
     open var yearId = 22
+    open fun setup() {}
     open fun partA(): String = ""
     open fun partB(): String = ""
     open fun reset() {}
+
     fun loadTrialInputAsString(): String {
         return InputHelpers.getContentsFromFile("/${yearId}/day${dayId}trial.txt")
     }
