585e6301d0a07e8bc21fdb99a2c52ca8c2e52ecc
Rolando Lopez <rolando.lopez@milwaukeetool.com>
2022-12-03 14:20:19 -0600

    More efficient

--- app/src/main/java/advent2022/day03/Y22D03.kt
+++ app/src/main/java/advent2022/day03/Y22D03.kt
@@ -6,15 +6,12 @@ class Y22D03 : AocDays() {
     override var dayId = 3
 
     override fun partA(): String {
-        val holder = mutableListOf<Char>()
+
+        var sum = 0L
         input.forEach { sack ->
             val firstHalf = sack.take(sack.length/2).toCharArray()
             val secondHalf = sack.takeLast(sack.length/2).toSet()
-            holder.add((firstHalf intersect secondHalf).first())
-        }
-        var sum = 0L
-        holder.forEach {
-           sum += priorityScore(it)
+            sum += priorityScore(((firstHalf intersect secondHalf).first()))
         }
 
         return "$sum"
@@ -30,19 +27,15 @@ class Y22D03 : AocDays() {
     override fun partB(): String {
         var sum = 0L
         val workingList = input.toMutableList()
-        val holder = mutableListOf<Char>()
         while(workingList.isNotEmpty()) {
             val trifecta = workingList.take(3).map { it.toCharArray() }
             val intersection = (trifecta[0] intersect trifecta[1].toSet()) intersect trifecta[2].toSet()
-            holder.add(intersection.first())
+            sum += priorityScore((intersection.first()))
             workingList.removeFirst()
             workingList.removeFirst()
             workingList.removeFirst()
         }
 
-        holder.forEach {
-            sum += priorityScore(it)
-        }
         return "$sum"
     }
 }
