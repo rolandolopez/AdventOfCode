74a6005dcab0959f1549eaa0a7ad61df5856c2ab
Rolando Lopez <rolando.lopez@milwaukeetool.com>
2022-12-05 08:36:28 -0600

    Day 5 Part B  ALSO GROSS!!!!

--- app/src/main/java/advent2022/day05/Y22D05.kt
+++ app/src/main/java/advent2022/day05/Y22D05.kt
@@ -1,46 +1,37 @@
 package advent2022.day05
 
 import com.twelfthnightdj.advent2021.AocDays
-import java.util.*
-import kotlin.collections.AbstractCollection
-import kotlin.collections.ArrayDeque
-import kotlin.collections.HashMap
 
 class Y22D05 : AocDays() {
     override var dayId = 5
 
+    val pods = mutableMapOf<Int, MutableList<String>>()
+
     override fun partA(): String {
-        val pods = mutableMapOf<Int, ArrayDeque<String>>()
         var instructionsReached = false
         input.forEach { line ->
             if (instructionsReached) {
-                val instr = line.replace("move.", "").replace("from.", "").replace("to.", "").split(".").map { it.toInt() }
-                repeat(instr[0]) { _ ->
-                    pods[instr[1]]?.first()?.let {
-                        pods[instr[1]]?.removeFirst()
-                        pods[instr[2]]?.add(0, it)
-                    }
-                }
+                val instr =
+                    line.replace("move.", "").replace("from.", "").replace("to.", "").split(".")
+                        .map { it.toInt() }
+                moveA(instr[0], instr[1], instr[2])
             } else {
                 if (line.isEmpty()) {
                     instructionsReached = true
                 } else {
                     line.forEachIndexed { index, c ->
                         if (c.isUpperCase()) {
-                            val podSpot = (index/4) + 1
+                            val podSpot = (index / 4) + 1
                             if (pods.containsKey(podSpot)) {
-                                pods[podSpot]?.addLast(c.toString())
+                                pods[podSpot]?.add(c.toString())
                             } else {
-                                val ad = ArrayDeque<String>()
-                                ad.add(c.toString())
-                                pods[podSpot] = ad
+                                pods[podSpot] = mutableListOf(c.toString())
                             }
                         }
                     }
                 }
             }
         }
-        println("pods: $pods")
         var answer = ""
         repeat(pods.size) {
             answer += pods[(it + 1)]?.first()
@@ -49,7 +40,58 @@ class Y22D05 : AocDays() {
         return answer
     }
 
+    private fun moveA(boxCount: Int, from: Int, to: Int) {
+        repeat(boxCount) { _ ->
+            pods[from]?.first()?.let {
+                pods[from]?.removeFirst()
+                pods[to]?.add(0, it)
+            }
+        }
+    }
+
+    private fun moveB(boxCount: Int, from: Int, to: Int) {
+        val moving = pods[from]?.subList(0, boxCount)?.toMutableList()
+        repeat(boxCount) { pods[from]?.removeFirst() }
+        moving?.let { boxes ->
+            pods[to]?.addAll(0, boxes)
+        }
+
+    }
+
+    override fun reset() {
+        pods.clear()
+    }
+
     override fun partB(): String {
-        return super.partB()
+        var instructionsReached = false
+        input.forEach { line ->
+            if (instructionsReached) {
+                val instr =
+                    line.replace("move.", "").replace("from.", "").replace("to.", "").split(".")
+                        .map { it.toInt() }
+                moveB(instr[0], instr[1], instr[2])
+            } else {
+                if (line.isEmpty()) {
+                    instructionsReached = true
+                } else {
+                    line.forEachIndexed { index, c ->
+                        if (c.isUpperCase()) {
+                            val podSpot = (index / 4) + 1
+                            if (pods.containsKey(podSpot)) {
+                                pods[podSpot]?.add(c.toString())
+                            } else {
+                                pods[podSpot] = mutableListOf(c.toString())
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        var answer = ""
+        repeat(pods.size) {
+            answer += pods[(it + 1)]?.first()
+        }
+
+        return answer
     }
 }
