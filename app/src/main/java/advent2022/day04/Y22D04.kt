9d079a40a9abb600c67220e1f32219d8bffda4be
Rolando Lopez <rolando.lopez@milwaukeetool.com>
2022-12-04 21:22:59 -0600

    Day 4 Part B

--- app/src/main/java/advent2022/day04/Y22D04.kt
+++ app/src/main/java/advent2022/day04/Y22D04.kt
@@ -10,7 +10,7 @@ class Y22D04 : AocDays() {
             val elves = e.split(",")
             val firstRange = elves[0].split("-").map { it.toInt() }
             val secondRange = elves[1].split("-").map { it.toInt() }
-            if (overlaps(firstRange, secondRange)) {
+            if (overlapsA(firstRange, secondRange)) {
                 total++
             }
         }
@@ -18,7 +18,7 @@ class Y22D04 : AocDays() {
         return "$total"
     }
 
-    private fun overlaps(firstRange: List<Int>, secondRange: List<Int>): Boolean {
+    private fun overlapsA(firstRange: List<Int>, secondRange: List<Int>): Boolean {
         if ((firstRange[0] == secondRange[0]) || (firstRange[1] == secondRange[1])) {
             return true
         }
@@ -29,7 +29,27 @@ class Y22D04 : AocDays() {
         }
     }
 
+    private fun overlapsB(firstRange: List<Int>, secondRange: List<Int>): Boolean {
+        if ((firstRange[0] == secondRange[0]) || (firstRange[1] == secondRange[1])) {
+            return true
+        }
+        return if (firstRange[0] < secondRange[0]) {
+            secondRange[0] <= firstRange[1]
+        } else {
+            firstRange[0] <= secondRange[1]
+        }
+    }
+
     override fun partB(): String {
-        return super.partB()
+        var total = 0
+        input.forEach { e ->
+            val elves = e.split(",")
+            val firstRange = elves[0].split("-").map { it.toInt() }
+            val secondRange = elves[1].split("-").map { it.toInt() }
+            if (overlapsB(firstRange, secondRange)) {
+                total++
+            }
+        }
+        return "$total"
     }
 }
