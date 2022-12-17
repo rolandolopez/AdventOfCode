4fbffdd05273f7b115ab5937d8856058ebe58825
Rolando Lopez <rolando.lopez@milwaukeetool.com>
2022-12-17 17:00:54 -0600

    Day 13 Part B with help from https://todd.ginsberg.com/post/advent-of-code/2022/day13/

--- app/src/main/java/advent2022/day13/Y22D13.kt
+++ app/src/main/java/advent2022/day13/Y22D13.kt
@@ -4,47 +4,51 @@ import com.twelfthnightdj.advent2021.AocDays
 
 class Y22D13 : AocDays() {
     override var dayId = 13
-    lateinit var packets: List<Packet>
+    lateinit var packets: MutableList<Pair<String, Packet>>
 
     override fun setup() {
-        packets = input.filter { it.isNotBlank() }.map { Packet.of(it) }
+        packets = input.filter { it.isNotBlank() }.map { Pair(it, Packet.of(it)) }.toMutableList()
     }
 
     override fun partA(): String {
         return packets.chunked(2).mapIndexed { index, list ->
-            if (list.first() < list.last()) index + 1 else 0
+            if (list.first().second < list.last().second) index + 1 else 0
         }.sum().toString()
     }
 
     override fun partB(): String {
-        return super.partB()
-    }
-
-    override fun reset() {
-        super.reset()
+        packets.add(Pair("[[2]]", Packet.of("[[2]]")))
+        packets.add(Pair("[[6]]", Packet.of("[[6]]")))
+        val sorted = packets.sortedBy { it.second }
+        return sorted.mapIndexed { index, pair ->
+            if (listOf("[[2]]", "[[6]]").contains(pair.first)) index + 1 else 0
+        }.filter { it > 0 }.let {
+            it[0] * it[1]
+        }.toString()
     }
 }
 
 sealed class Packet : Comparable<Packet> {
     companion object {
-        fun of(input: String): Packet =
-            of(
+        fun of(input: String): Packet {
+            return of(
                 input.split("""((?<=[\[\],])|(?=[\[\],]))""".toRegex())
                     .filter { it.isNotBlank() }
                     .filter { it != "," }
                     .iterator()
             )
+        }
 
         private fun of(input: Iterator<String>): Packet {
-            val packets = mutableListOf<Packet>()
+            val localPackets = mutableListOf<Packet>()
             while(input.hasNext()) {
                 when (val symbol = input.next()) {
-                    "]" -> return ListPacket(packets)
-                    "[" -> packets.add(of(input))
-                    else -> packets.add(IntPacket(symbol.toInt()))
+                    "]" -> return ListPacket(localPackets)
+                    "[" -> localPackets.add(of(input))
+                    else -> localPackets.add(IntPacket(symbol.toInt()))
                 }
             }
-            return ListPacket(packets)
+            return ListPacket(localPackets)
         }
     }
 }
