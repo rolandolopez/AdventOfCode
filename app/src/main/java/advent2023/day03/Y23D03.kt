512d46406e79acb8f024ba353d15c8721f1485a3
Rolando Lopez <rolando.lopez@milwaukeetool.com>
2023-12-03 23:55:06 -0600

    2023 Day 3 Part B

--- app/src/main/java/advent2023/day03/Y23D03.kt
+++ app/src/main/java/advent2023/day03/Y23D03.kt
@@ -2,15 +2,15 @@ package advent2023.day03
 
 import com.twelfthnightdj.advent2021.AocDays
 import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.async
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.launch
-import java.util.Observable
 
 class Y23D03 : AocDays() {
     override var dayId = 3
     var grid: MutableSet<String> = mutableSetOf()
+    var gridB: MutableMap<String, Int> = mutableMapOf()
     var sum = 0
+    var sumB = 0
 
     override fun partA(): String {
         input.forEachIndexed { index, line ->
@@ -35,12 +35,11 @@ class Y23D03 : AocDays() {
                 while (parts != null) {
                     if (isPartNumber(index, parts.range)) {
                         sum += parts.value.toInt()
-                        println("sum: $sum")
                     }
                     parts = parts.next()
                 }
             }
-            println("final: $sum")
+            println("part A: $sum")
         }
     }
 
@@ -60,10 +59,81 @@ class Y23D03 : AocDays() {
     }
 
     override fun partB(): String {
-        return super.partB()
+        input.forEachIndexed { index, line ->
+            var all = "\\*".toRegex().find(line)
+            while (all != null) {
+                gridB["$index#${all.range.first}"] = 0
+                all = all.next()
+            }
+        }
+
+        GlobalScope.launch {
+            checkB()
+        }
+        return sum.toString()
+    }
+
+    private fun gearCalculation(x: Int, range: IntRange, v: Int): Int {
+        var getOut = 0
+        ((range.first - 1)..(range.last + 1)).forEach {
+             gridB.computeIfPresent("${x - 1}#$it") { _, value ->
+                 if (value != 0) {
+                     getOut = value * v
+                     value * v
+                 } else {
+                     v
+                 }
+             }
+            if (getOut > 0) return getOut
+            gridB.computeIfPresent("${x + 1}#$it") { _, value ->
+                 if (value != 0) {
+                     getOut = value * v
+                     value * v
+                 } else {
+                     v
+                 }
+             }
+            if (getOut > 0) return getOut
+        }
+        gridB.computeIfPresent("$x#${range.first - 1}") { _ , value ->
+            if (value != 0) {
+                getOut = value * v
+                value * v
+            } else {
+                v
+            }
+        }
+        if (getOut > 0) return getOut
+
+        gridB.computeIfPresent("$x#${range.last + 1}") { _ , value ->
+            if (value != 0) {
+                getOut = value * v
+                value * v
+            } else {
+                v
+            }
+        }
+        if (getOut > 0) return getOut
+
+        return 0
+    }
+
+    private suspend fun checkB() {
+        coroutineScope {
+            input.forEachIndexed { index, line ->
+                var parts = "\\d+".toRegex().find(line)
+                while (parts != null) {
+                    val t = gearCalculation(index, parts.range, parts.value.toInt())
+                    sumB += t
+                    parts = parts.next()
+                }
+            }
+            println("partB: $sumB")
+        }
     }
 
     override fun reset() {
+        sum = 0
         super.reset()
     }
 }
