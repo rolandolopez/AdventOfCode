a9ab5bfa04ea098af094be08d2a93216b9cbdf32
Rolando Lopez <rolando.lopez@milwaukeetool.com>
2024-12-06 21:26:58 -0600

    Day 6 2024 Part B!

--- app/src/main/java/advent2024/day06/Y24D06.kt
+++ app/src/main/java/advent2024/day06/Y24D06.kt
@@ -1,12 +1,17 @@
 package advent2024.day06
 
 import com.twelfthnightdj.advent2021.AocDays
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 import utils.Direction
 
 class Y24D06 : AocDays() {
     override var dayId = 6
     private val grid = mutableMapOf<String, Char>()
     private val path = mutableSetOf<String>()
+    private val pathB = mutableSetOf<Pair<String, Direction>>()
+    private var startingX = 0
+    private var startingY = 0
     private var maxX = 0
     private var maxY = 0
     private var currentX = -1
@@ -24,6 +29,9 @@ class Y24D06 : AocDays() {
                 if (c == '^') {
                     currentX = x
                     currentY = y
+                    startingX = x
+                    startingY = y
+                    grid["$x#$y"] = '.'
                     path.add("$x#$y")
                 }
             }
@@ -50,6 +58,58 @@ class Y24D06 : AocDays() {
             }
         }
 
+    }
+
+    override fun reset() {
+        currentX = startingX
+        currentY = startingY
+        super.reset()
+    }
+
+    override fun partB(): String {
+        GlobalScope.launch {
+            var total = 0
+            (0..maxX).forEach { x ->
+                (0..maxY).forEach yLoop@{ y ->
+                    val key = "$x#$y"
+                    if (grid[key] == '#') {
+                        return@yLoop
+                    }
+                    grid[key] = '#'
+                    currentX = startingX
+                    currentY = startingY
+                    direction = Direction.NORTH
+                    if (walkTheMaze()) {
+                        total++
+                    }
+                    grid[key] = '.'
+                }
+            }
+            println("final total: $total")
+        }
+        return ""
+    }
+
+    private fun walkTheMaze():Boolean {
+        pathB.clear()
+        pathB.add(Pair("$currentX#$currentY", Direction.NORTH))
+        while (true) {
+            previewMove()
+            val key = "$potentialX#$potentialY"
+            if (grid[key] == null) {
+                return false
+            }
+            if (grid[key] == '#') {
+                turn()
+            } else {
+                currentX = potentialX
+                currentY = potentialY
+                if (pathB.contains(Pair("$currentX#$currentY", direction))) {
+                    return true
+                }
+                pathB.add(Pair("$currentX#$currentY", direction))
+            }
+        }
     }
 
     private fun previewMove() {
