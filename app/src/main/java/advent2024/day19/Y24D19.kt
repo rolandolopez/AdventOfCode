4f9ba8424926f583f88b76a2b46685851cba0177
Rolando Lopez <rolando.lopez@milwaukeetool.com>
2024-12-19 18:11:50 -0600

    Day 19, 2024 Part B, does not work

--- app/src/main/java/advent2024/day19/Y24D19.kt
+++ app/src/main/java/advent2024/day19/Y24D19.kt
@@ -1,11 +1,14 @@
 package advent2024.day19
 
 import com.twelfthnightdj.advent2021.AocDays
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class Y24D19 : AocDays() {
     override var dayId = 19
     private var towels = mutableListOf<String>()
     private var designs = mutableListOf<String>()
+    private var total = 0
 
     override fun setup() {
 
@@ -22,20 +25,46 @@ class Y24D19 : AocDays() {
     }
 
     override fun partA(): String {
-        var total = 0
         designs.forEach { design ->
-            if (checkForDesign(design)) total++.also { println("$design is possible") }
+            if (checkForDesignA(design)) total++
         }
         return total.toString()
     }
 
-    fun checkForDesign(design: String): Boolean {
+    override fun reset() {
+        total = 0
+    }
+
+    fun checkForDesignA(design: String): Boolean {
         towels.forEachIndexed { index, towel ->
             if (design.indexOf(towel) == 0) {
                 if (design.length == towel.length) return true
-                if (checkForDesign(design.substring(towel.length))) return true
+                if (checkForDesignA(design.substring(towel.length))) return true
             }
         }
         return false
+    }
+
+    override fun partB(): String {
+        GlobalScope.launch {
+            designs.forEach { design ->
+                checkForDesignB(design)
+                println("completed check for $design with total so far: $total")
+            }
+            println("total for part B: $total")
+        }
+        return total.toString()
+    }
+
+    fun checkForDesignB(design: String){
+        towels.forEachIndexed { index, towel ->
+            if (design.indexOf(towel) == 0) {
+                if (design.length == towel.length) {
+                    total++
+                    return
+                }
+                checkForDesignB(design.substring(towel.length))
+            }
+        }
     }
 }
