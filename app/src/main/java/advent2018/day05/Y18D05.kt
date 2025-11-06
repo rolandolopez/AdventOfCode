6e6f6565c40841a937a1d967921981464e89a96c
Rolando Lopez <rolando.lopez@milwaukeetool.com>
2025-11-06 11:17:40 -0600

    Year 2018, Day 5, Part B

--- app/src/main/java/advent2018/day05/Y18D05.kt
+++ app/src/main/java/advent2018/day05/Y18D05.kt
@@ -3,12 +3,31 @@ package advent2018.day05
 import com.twelfthnightdj.advent2021.AocDays
 import kotlin.math.abs
 import kotlin.math.max
+import kotlin.math.min
 
 class Y18D05 : AocDays() {
     override var dayId = 5
 
     override fun partA(): String {
         val chars = inputAsString.toCharArray().map { it.code }.toMutableList()
+
+        return condense(chars).toString()
+    }
+
+    override fun partB(): String {
+        val chars = inputAsString.toCharArray().map { it.code }.toMutableList()
+        var winner = chars.size
+        var filteredChars: MutableList<Int>
+        for (i in 65..90) {
+            filteredChars = chars.filter {
+                it != i && it != (i + 32)
+            }.toMutableList()
+            winner = min(condense(filteredChars), winner)
+        }
+        return winner.toString()
+    }
+
+    private fun condense(chars: MutableList<Int>): Int {
         var index = 0
         while (true) {
             index = max(0, index)
@@ -23,6 +42,6 @@ class Y18D05 : AocDays() {
                 index++
             }
         }
-        return chars.size.toString()
+        return chars.size
     }
 }
