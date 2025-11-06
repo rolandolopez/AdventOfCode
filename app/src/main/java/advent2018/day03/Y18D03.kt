06f66f0f84e202f480b2af3265b206697b9171e3
Rolando Lopez <rolando.lopez@milwaukeetool.com>
2025-11-05 22:22:45 -0600

    Year 2018, Day 3, Part B

--- app/src/main/java/advent2018/day03/Y18D03.kt
+++ app/src/main/java/advent2018/day03/Y18D03.kt
@@ -4,11 +4,11 @@ import com.twelfthnightdj.advent2021.AocDays
 
 class Y18D03 : AocDays() {
     override var dayId = 3
-    var grid: MutableMap<String, Int> = mutableMapOf()
 
     override fun partA(): String {
+        val grid: MutableMap<String, Int> = mutableMapOf()
         input.forEach { str ->
-            val (id, _, startPointPlus, area) = str.split(" ")
+            val (_, _, startPointPlus, area) = str.split(" ")
             val startPoint = startPointPlus.removeSuffix(":")
             val (x, y) = startPoint.split(",").map { it.toInt() }
             val (dx, dy) = area.split("x").map { it.toInt() }
@@ -22,9 +22,40 @@ class Y18D03 : AocDays() {
                     }
                 }
             }
-            println("$str: $id, $startPoint, $area")
         }
 
         return grid.count { it.value > 1 }.toString()
+    }
+
+    override fun partB(): String {
+        val grid: MutableMap<String, MutableSet<Int>> = mutableMapOf()
+        val allIds: MutableSet<Int> = mutableSetOf()
+        repeat(input.size) { idx ->
+            allIds.add(idx + 1)
+        }
+        input.forEach { str ->
+            val (idString, _, startPointPlus, area) = str.split(" ")
+            val id = idString.removePrefix("#").toInt()
+            val startPoint = startPointPlus.removeSuffix(":")
+            val (x, y) = startPoint.split(",").map { it.toInt() }
+            val (dx, dy) = area.split("x").map { it.toInt() }
+            repeat(dx) { xChange ->
+                repeat(dy) { yChange ->
+                    val index = "${x + xChange}#${y + yChange}"
+                    if (grid.containsKey(index)) {
+                        grid.computeIfPresent(index) { _, value ->
+                            value.add(id)
+                            value.forEach { foundIds ->
+                                allIds.remove(foundIds)
+                            }
+                            value
+                        }
+                    } else {
+                        grid[index] = mutableSetOf(id)
+                    }
+                }
+            }
+        }
+        return allIds.first().toString()
     }
 }
