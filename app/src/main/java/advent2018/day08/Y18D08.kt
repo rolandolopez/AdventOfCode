2c2441fb291d1e3c54612e4211b079689ebb86a0
Rolando Lopez <rolando.lopez@milwaukeetool.com>
2025-11-10 18:42:49 -0600

    Year 2018, Day 8, Part B Works for the trial input but not for the actual input.  :(

--- app/src/main/java/advent2018/day08/Y18D08.kt
+++ app/src/main/java/advent2018/day08/Y18D08.kt
@@ -8,23 +8,70 @@ class Y18D08 : AocDays() {
     var metaDataSum = 0
     var currentIndex = 0
     var nums: List<Int> = listOf()
+    override fun setup() {
+        nums = inputAsString.split(" ").map { it.toInt() }
+    }
 
     override fun partA(): String {
-        nums = inputAsString.split(" ").map { it.toInt() }
         while (currentIndex < nums.size) {
-            createNode()
+            createNodeA()
         }
         return metaDataSum.toString()
     }
 
-    private fun createNode() {
+    override fun reset() {
+        metaDataSum = 0
+        currentIndex = 0
+    }
+
+    override fun partB(): String {
+        var bSum = 0
+        while (currentIndex < nums.size) {
+             bSum += createNodeB()
+        }
+        return bSum.toString()
+    }
+
+    private fun createNodeA() {
         val numOfChildNodes = nums[currentIndex++]
         val numOfMetaDataEntries = nums[currentIndex++]
         repeat(numOfChildNodes) {
-            createNode()
+            createNodeA()
         }
         repeat(numOfMetaDataEntries) {
             metaDataSum += nums[currentIndex++]
         }
+    }
+
+    private fun createNodeB(): Int {
+        val numOfChildNodes = nums[currentIndex++]
+        val numOfMetaDataEntries = nums[currentIndex++]
+        var sum = 0
+        val childNodes: MutableList<Int> = mutableListOf()
+        repeat(numOfChildNodes) {
+            childNodes.add(createNodeB())
+        }
+        if (numOfChildNodes == 0) {
+            repeat(numOfMetaDataEntries) {
+                sum += nums[currentIndex++]
+            }
+            println("returning zero child: $sum")
+            return sum
+        } else {
+            val indexes: MutableList<Int> = mutableListOf()
+            repeat(numOfMetaDataEntries) {
+                indexes.add(nums[currentIndex++])
+            }
+            indexes.forEach { idx ->
+                println("*******  $idx < ${childNodes.size - 1}  sum: $sum")
+                if (idx < childNodes.size && idx > 0) {
+                    println("adding: ${childNodes[idx - 1]}")
+                    sum += childNodes[idx - 1]
+                }
+            }
+            println("returning the else of childnodes${childNodes} of index: $indexes: $sum")
+            return sum
+        }
+
     }
 }
