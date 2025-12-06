59d131ad306313dd028699410936ec88b4969001
Rolando Lopez <rolando.lopez@milwaukeetool.com>
2025-12-06 10:17:52 -0600

    2025 Day 5 Part B (Not working)

--- app/src/main/java/advent2025/Y25D05.kt
+++ app/src/main/java/advent2025/Y25D05.kt
@@ -4,10 +4,10 @@ import com.twelfthnightdj.advent2021.AocDays
 
 class Y25D05 : AocDays() {
     override var dayId = 5
-    var ranges = mutableSetOf<LongRange>()
-    var total = 0
 
     override fun partA(): String {
+        val ranges = mutableSetOf<LongRange>()
+        var total = 0
         var inRanges = true
         input.forEach { str ->
             if (str.isEmpty()) {
@@ -21,5 +21,27 @@ class Y25D05 : AocDays() {
             }
         }
         return total.toString()
+    }
+
+    override fun partB(): String {
+        val ranges = mutableSetOf<LongRange>()
+        var inRanges = true
+        val all = mutableSetOf<Long>()
+        input.forEach { str ->
+            if (str.isEmpty()) {
+                inRanges = false
+            } else if (inRanges) {
+                val (start, end) = str.split("-").map { it.toLong() }
+                ranges.add(LongRange(start, end))
+            } else {
+                ranges.forEach {
+                    if (it.contains(str.toLong())) {
+                        all.addAll(it.toSet())
+                    }
+                }
+            }
+        }
+
+        return all.size.toString()
     }
 }
