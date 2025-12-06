44a7e7e1b2039454cc0a69afd800eb18af22a9fc
Rolando Lopez <rolando.lopez@milwaukeetool.com>
2025-12-06 17:15:01 -0600

    2025 Day 6 Part B (this is gross)

--- app/src/main/java/advent2025/Y25D06.kt
+++ app/src/main/java/advent2025/Y25D06.kt
@@ -7,7 +7,6 @@ class Y25D06 : AocDays() {
 
     override fun partA(): String {
         val factorsCount = input.size
-        println(factorsCount)
         var operation = listOf<String>()
         val grid = mutableListOf<List<Long>>()
         input.forEachIndexed { idx, str ->
@@ -29,6 +28,52 @@ class Y25D06 : AocDays() {
                 factors.fold(1L) {acc, element -> acc * element}
             }
         }
+        return total.toString()
+    }
+
+    override fun partB(): String {
+        var operation = ' '
+        val width = input[0].length + 2
+        val height = input.size
+
+        var total = 0L
+        val factors = mutableListOf<Long>()
+        val newInput = input.toMutableList()
+        var starter = input[0]
+        starter = starter.prependIndent("  ")
+        newInput[0] = starter
+        for (w in 0 until width) {
+            var factor = ""
+            for (h in 0 until height) {
+                if (w < newInput[h].length) {
+                    if (h == height - 1) {
+                        if (newInput[h][w] != ' ') {
+                            operation = newInput[h][w]
+                        }
+                    } else {
+                        if (newInput[h][w] != ' ') {
+                            factor += newInput[h][w]
+                        }
+                    }
+                }
+            }
+            if (factor == "") {
+                total += if (operation == '+') {
+                    factors.fold(0L) {acc, element -> acc + element}
+                } else {
+                    factors.fold(1L) {acc, element -> acc * element}
+                }
+                factors.clear()
+            } else {
+                factors.add(factor.toLong())
+            }
+        }
+        total += if (operation == '+') {
+            factors.fold(0L) {acc, element -> acc + element}
+        } else {
+            factors.fold(1L) {acc, element -> acc * element}
+        }
+
         return total.toString()
     }
 }
